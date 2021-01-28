59b7b9e5a8e01ff7c86f3cd2b8f7143ab52bf360
Silas <silas.truong.goldenowl@gmail.com>
2021-01-28 13:50:02 +0700

    add env for middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,33 +2,33 @@ import axios from "axios";
 
 export function getStaskDatabase(id) {
   return axios.get(
-    `https://todos-758f8-default-rtdb.firebaseio.com/${id}.json?auth=tMP42bwtqN09D5oxY12YkLsDCJZZEq9RWMGYGLNA`
+    `https://${process.env.REACT_APP_FIREBASE_DOMAIN}/${id}.json?auth=${process.env.REACT_APP_FIREBASE_AUTH}`
   );
 }
 
 export function putFullStaskDatabase(id, data) {
   return axios.put(
-    `https://todos-758f8-default-rtdb.firebaseio.com/${id}/stask.json/?auth=tMP42bwtqN09D5oxY12YkLsDCJZZEq9RWMGYGLNA`,
+    `https://${process.env.REACT_APP_FIREBASE_DOMAIN}/${id}/stask.json/?auth=${process.env.REACT_APP_FIREBASE_AUTH}`,
     data
   );
 }
 
 export function putStaskDatabase(id, data) {
   return axios.put(
-    `https://todos-758f8-default-rtdb.firebaseio.com/${id}/stask/${data.id}.json/?auth=tMP42bwtqN09D5oxY12YkLsDCJZZEq9RWMGYGLNA`,
+    `https://${process.env.REACT_APP_FIREBASE_DOMAIN}/${id}/stask/${data.id}.json/?auth=${process.env.REACT_APP_FIREBASE_AUTH}`,
     data
   );
 }
 
 export function deleteStaskDatabase(id, data) {
   return axios.delete(
-    `https://todos-758f8-default-rtdb.firebaseio.com/${id}/stask/${data.id}.json?auth=tMP42bwtqN09D5oxY12YkLsDCJZZEq9RWMGYGLNA`
+    `https://${process.env.REACT_APP_FIREBASE_DOMAIN}/${id}/stask/${data.id}.json?auth=${process.env.REACT_APP_FIREBASE_AUTH}`
   );
 }
 
 export function postStaskDatabase(id, idStask, data) {
   return axios.post(
-    `https://todos-758f8-default-rtdb.firebaseio.com/${id}/stask/${idStask}.json/?auth=tMP42bwtqN09D5oxY12YkLsDCJZZEq9RWMGYGLNA`,
+    `https://${process.env.REACT_APP_FIREBASE_DOMAIN}/${id}/stask/${idStask}.json/?auth=${process.env.REACT_APP_FIREBASE_AUTH}`,
     data
   );
 }
